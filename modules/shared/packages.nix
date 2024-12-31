913f795d50cb86c9c23cbcdcb35fe91bb9931b2a
Tharaka De Silva <tharaka.uo@gmail.com>
2024-12-31 15:48:52 +0100

    Refactor shared package list in Nix configuration
    - Removed several outdated and unnecessary packages for a cleaner setup.
    - Added new development tools including 'bazelisk', 'bottom', 'buildifier', and 'cowsay'.
    - Updated existing packages to enhance functionality and maintainability.
    - Organized the package list for better clarity and usability.

--- modules/shared/packages.nix
+++ modules/shared/packages.nix
@@ -2,60 +2,40 @@
 
 with pkgs; [
   # General packages for development and system management
-  alacritty
-  aspell
-  aspellDicts.en
-  bash-completion
   bat
-  btop
-  coreutils
-  killall
-  neofetch
-  openssh
-  sqlite
-  wget
-  zip
-
-  # Encryption and security tools
-  age
-  age-plugin-yubikey
-  gnupg
-  libfido2
-
-  # Cloud-related tools and SDKs
+  bazelisk
+  bottom
+  buildifier
+  bun
+  cowsay
+  delta
   docker
-  docker-compose
-
-  # Media-related packages
-  emacs-all-the-icons-fonts
-  dejavu_fonts
-  ffmpeg
+  duf
+  dust
+  eza
   fd
-  font-awesome
-  hack-font
-  noto-fonts
-  noto-fonts-emoji
-  meslo-lgs-nf
-
-  # Node.js development tools
-  nodePackages.npm # globally install npm
-  nodePackages.prettier
-  nodejs
-
-  # Text and terminal utilities
-  htop
-  hunspell
-  iftop
-  jetbrains-mono
+  ffmpeg
+  git
+  git-lfs
+  gnupg
+  go
+  ibazel
   jq
-  ripgrep
-  tree
-  tmux
-  unrar
-  unzip
-  zsh-powerlevel10k
-
-  # Python packages
+  maven
+  neovim
+  node
+  nodePackages.eas-cli
+  nodePackages.npm
+  openjdk
+  pinentry
+  procs
   python3
-  virtualenv
+  ripgrep
+  rm-improved
+  rsync
+  supabase
+  thefuck
+  tmux
+  zoxide
+  zsh
 ]
