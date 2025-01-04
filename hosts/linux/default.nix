3211de905dd276fbd865e0405e47586adbd8847e
Tharaka De Silva <tharaka.uo@gmail.com>
2025-01-04 22:55:58 +0100

    Refactor NixOS home-manager configuration for Linux
    - Updated the `default.nix` file to streamline the Nix configuration by uncommenting and organizing the `nix` settings, including trusted users, substituters, and garbage collection options.
    - Enabled `home-manager` for user management, ensuring a more cohesive setup.
    - Introduced experimental features for Nix, enhancing the overall functionality and user experience.
    
    These changes collectively improve the clarity and usability of the home-manager configuration for Linux systems.

--- hosts/linux/default.nix
+++ hosts/linux/default.nix
@@ -24,47 +24,36 @@ in {
     '';
   };
 
-  programs.home-manager.enable = true;
-
   # Setup user, packages, programs
-  # nix = {
-  #   package = pkgs.nix;
-  #   settings = {
-  #     trusted-users = ["@admin" "${user}"];
-  #     substituters = ["https://nix-community.cachix.org" "https://cache.nixos.org"];
-  #     trusted-public-keys = ["cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="];
-  #   };
+  nix = {
+    package = pkgs.nix;
+    settings = {
+      trusted-users = ["@admin" "${user}"];
+      substituters = ["https://nix-community.cachix.org" "https://cache.nixos.org"];
+      trusted-public-keys = ["cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="];
+    };
 
-  #   gc = {
-  #     user = "root";
-  #     automatic = true;
-  #     interval = {
-  #       Weekday = 0;
-  #       Hour = 2;
-  #       Minute = 0;
-  #     };
-  #     options = "--delete-older-than 7d";
-  #   };
+    gc = {
+      user = "tdesilva";
+      automatic = true;
+      interval = {
+        Weekday = 0;
+        Hour = 2;
+        Minute = 0;
+      };
+      options = "--delete-older-than 7d";
+    };
 
-  #   optimise = {
-  #     automatic = true;
-  #   };
+    optimise = {
+      automatic = true;
+    };
 
-  #   extraOptions = ''
-  #     experimental-features = nix-command flakes
-  #     keep-outputs = true
-  #     keep-derivations = true
-  #   '';
-  # };
+    extraOptions = ''
+      experimental-features = nix-command flakes
+      keep-outputs = true
+      keep-derivations = true
+    '';
+  };
 
-  # Load configuration that is shared across systems
-  # environment.systemPackages = with pkgs;
-  #   [
-  #     # agenix.packages."${pkgs.system}".default
-  #   ]
-  #   ++ (import ../../modules/shared/packages.nix {inherit pkgs;});
-
-  # fonts.packages = with pkgs; [
-  #   nerd-fonts.jetbrains-mono
-  # ];
+  programs.home-manager.enable = true;
 }
