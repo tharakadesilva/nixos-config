05ccfea546972caf6306dd73da8cac119cffeb10
Tharaka De Silva <tharaka.uo@gmail.com>
2025-01-05 23:09:39 +0100

    Remove commented-out auto-correction line from oh-my-zsh configuration in home-manager.nix

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -453,9 +453,6 @@ in {
     oh-my-zsh = {
       enable = true;
       extraConfig = ''
-        # Uncomment the following line to enable command auto-correction.
-        ENABLE_CORRECTION="true"
-
         # Uncomment the following line to display red dots whilst waiting for completion.
         # You can also set it to another string to have that shown instead of the default red dots.
         # e.g. COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
