4aa4c65a3bb9fe49df6e0aed5eeebc107351b0f6
Tharaka De Silva <tharaka.uo@gmail.com>
2025-04-09 12:47:54 +0200

    Add JAVA_HOME environment variable in home-manager.nix
    - Set JAVA_HOME to the OpenJDK package for improved Java development support.
    - Ensured consistent environment variable configuration for users.

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -543,6 +543,7 @@ in {
       export EDITOR="cursor"
       export LANG=en_US.UTF-8
       export LC_ALL=en_US.UTF-8
+      export JAVA_HOME=${pkgs.openjdk}
       export ANDROID_HOME=$HOME/Library/Android/sdk
       export PATH=$PATH:$ANDROID_HOME/emulator
       export PATH=$PATH:$ANDROID_HOME/platform-tools
