e071838ecfb47841e11b7db818e100bdad1203c5
Tharaka De Silva <tharaka.uo@gmail.com>
2025-05-10 08:46:01 +0200

    Add Go binary path to environment variables in home-manager.nix

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -547,6 +547,7 @@ in {
       export ANDROID_HOME=$HOME/Library/Android/sdk
       export PATH=$PATH:$ANDROID_HOME/emulator
       export PATH=$PATH:$ANDROID_HOME/platform-tools
+      export PATH=$PATH:$HOME/go/bin
     '';
 
     shellAliases = {
