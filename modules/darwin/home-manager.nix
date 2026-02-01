6a71bf056ace31b82c2b380cc0edacf29b90c821
Tharaka De Silva <tharaka.uo@gmail.com>
2026-02-01 14:43:52 +0100

    refactor(darwin): apply nixfmt formatting and update VS Code config
    - Format with nixfmt (inherit style, let/in formatting)
    - Remove VS Code extensions: Biome, ESLint, Prettier
    - Add amp.experimental.modes setting
    - Add RadonIDE device location settings
    - Add errorLens cSpell exclusion
    - Remove editor formatOnSave/Type/Paste settings
    - Remove language-specific defaultFormatter settings

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -4,13 +4,22 @@
   lib,
   home-manager,
   ...
-}: let
+}:
+let
   user = "tharakadesilva";
   nixRoot = "/nix/var/nix/profiles/default";
-  sharedPrograms = import ../shared/home-manager.nix {inherit config pkgs lib nixRoot;};
-  sharedFiles = import ../shared/files.nix {inherit config pkgs;};
-  additionalFiles = import ./files.nix {inherit user config pkgs;};
-in {
+  sharedPrograms = import ../shared/home-manager.nix {
+    inherit
+      config
+      pkgs
+      lib
+      nixRoot
+      ;
+  };
+  sharedFiles = import ../shared/files.nix { inherit config pkgs; };
+  additionalFiles = import ./files.nix { inherit user config pkgs; };
+in
+{
   # It me
   users.users.${user} = {
     name = "${user}";
@@ -33,7 +42,7 @@ in {
       "ruby"
       # End of Expo requirements
     ];
-    casks = pkgs.callPackage ./casks.nix {};
+    casks = pkgs.callPackage ./casks.nix { };
     onActivation = {
       autoUpdate = true;
       upgrade = true;
@@ -62,32 +71,31 @@ in {
   # Enable home-manager
   home-manager = {
     useGlobalPkgs = true;
-    users.${user} = {
-      pkgs,
-      config,
-      lib,
-      ...
-    }: {
-      home = {
-        enableNixpkgsReleaseCheck = false;
-        packages =
-          [
+    users.${user} =
+      {
+        pkgs,
+        config,
+        lib,
+        ...
+      }:
+      {
+        home = {
+          enableNixpkgsReleaseCheck = false;
+          packages = [
             (pkgs.python3.withPackages (ppkgs: [
               ppkgs.setuptools
             ]))
           ]
-          ++ (pkgs.callPackage ./packages.nix {});
-        file = lib.mkMerge [
-          sharedFiles
-          additionalFiles
-        ];
+          ++ (pkgs.callPackage ./packages.nix { });
+          file = lib.mkMerge [
+            sharedFiles
+            additionalFiles
+          ];
 
-        stateVersion = "23.11";
-      };
+          stateVersion = "23.11";
+        };
 
-      programs =
-        sharedPrograms
-        // {
+        programs = sharedPrograms // {
           vscode = {
             enable = true;
             mutableExtensionsDir = true;
@@ -99,12 +107,10 @@ in {
                 pkgs.vscode-extensions.bazelbuild.vscode-bazel # Bazel
                 # TODO: Create a ticket to add this extension
                 # pkgs.vscode-extensions.blazejkustra.react-compiler-marker
-                pkgs.vscode-extensions.biomejs.biome # Biome
                 pkgs.vscode-extensions.streetsidesoftware.code-spell-checker # Code Spell Checker
                 pkgs.vscode-extensions.ms-vscode-remote.remote-containers # Dev Containers
                 pkgs.vscode-extensions.ms-azuretools.vscode-docker # Docker
                 pkgs.vscode-extensions.editorconfig.editorconfig # EditorConfig
-                pkgs.vscode-extensions.dbaeumer.vscode-eslint # ESLint
                 # TODO: Create a ticket to add this extension
                 # pkgs.vscode-extensions.bdsoftware.format-on-auto-save # Format on Auto Save
                 pkgs.vscode-extensions.github.vscode-pull-request-github # GitHub Pull Request
@@ -115,7 +121,6 @@ in {
                 pkgs.vscode-extensions.ms-python.isort # isort (Import Sorting)
                 pkgs.vscode-extensions.pkief.material-icon-theme # Material Icon Theme
                 pkgs.vscode-extensions.christian-kohler.path-intellisense # Path Intellisense
-                pkgs.vscode-extensions.esbenp.prettier-vscode # Prettier
                 pkgs.vscode-extensions.ms-python.vscode-pylance # Pylance
                 pkgs.vscode-extensions.ms-python.python # Python
                 # TODO: https://github.com/NixOS/nixpkgs/issues/371247
@@ -130,14 +135,17 @@ in {
                 pkgs.vscode-extensions.yoavbls.pretty-ts-errors
               ];
               userSettings = {
-                "amp.url" = "https://ampcode.com/";
-                "amp.tab.enabled" = true;
+                "amp.experimental.modes" = [
+                  "bombadil"
+                  "deep"
+                ];
                 "amp.permissions" = [
                   {
                     "tool" = "*";
                     "action" = "allow";
                   }
                 ];
+                "amp.url" = "https://ampcode.com/";
                 "bazel.buildifierExecutable" = "${pkgs.buildifier}/bin/buildifier";
                 "bazel.buildifierFixOnFormat" = true;
                 "cSpell.userWords" = [
@@ -148,23 +156,21 @@ in {
                 "editor.inlineSuggest.enabled" = true;
                 "editor.fontFamily" = "'JetbrainsMono Nerd Font Mono'";
                 "editor.fontSize" = 13;
-                "editor.formatOnPaste" = true;
-                "editor.formatOnSave" = true;
-                "editor.formatOnType" = true;
                 "editor.suggestSelection" = "first";
+                "errorLens.excludeBySource" = [
+                  "cSpell"
+                ];
                 "explorer.confirmDelete" = false;
                 "files.autoSave" = "afterDelay";
                 "files.autoSaveDelay" = 2000;
-                "git" = {
-                  "autofetch" = true;
-                  "confirmSync" = false;
-                };
-                "github.copilot.enable" = {
-                  "*" = false;
-                };
-                "json.schemas" = [];
+                "json.schemas" = [ ];
                 "RadonIDE.deviceControl.stopPreviousDevices" = "true";
                 "RadonIDE.deviceSettings.appearance" = "dark";
+                "RadonIDE.deviceSettings.location" = {
+                  "latitude" = 52.378901;
+                  "longitude" =  4.9005805;
+                  "isDisabled" = false;
+                };
                 "RadonIDE.deviceSettings.replaysEnabled" = true;
                 "RadonIDE.deviceSettings.showTouches" = true;
                 "RadonIDE.general.defaultMultimediaSavingLocation" = "~/Downloads";
@@ -203,34 +209,16 @@ in {
                   "!Sub"
                   "!Sub sequence"
                 ];
-                "yaml.schemas" = {};
-                "[javascript]" = {
-                  "editor.defaultFormatter" = "vscode.typescript-language-features";
-                };
-                "[javascriptreact]" = {
-                  "editor.defaultFormatter" = "vscode.typescript-language-features";
-                };
-                "[json]" = {
-                  "editor.defaultFormatter" = "vscode.json-language-features";
-                };
-                "[jsonc]" = {
-                  "editor.defaultFormatter" = "vscode.json-language-features";
-                };
-                "[typescript]" = {
-                  "editor.defaultFormatter" = "vscode.typescript-language-features";
-                };
-                "[typescriptreact]" = {
-                  "editor.defaultFormatter" = "vscode.typescript-language-features";
-                };
+                "yaml.schemas" = { };
               };
             };
           };
         };
 
-      services.gpg-agent = {
-        enable = true;
-        pinentry.package = pkgs.pinentry_mac;
+        services.gpg-agent = {
+          enable = true;
+          pinentry.package = pkgs.pinentry_mac;
+        };
       };
-    };
   };
 }
